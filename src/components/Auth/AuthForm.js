53751ae5348db85f2de491ecfcb7ad5628a02089
Mykola Matsiuk <mykola.matsiuk@gmail.com>
2021-11-05 17:06:13 +0200

    authentication, JWT in localStorage

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,39 +1,129 @@
-import { useState } from 'react';
+import { useState, useRef, useContext } from 'react';
+import { useHistory } from 'react-router';
 
+import AuthContext from '../../store/auth-context';
+import { appKey } from '../../appKey';
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
-  const [isLogin, setIsLogin] = useState(true);
+	const emailInputRef = useRef();
+	const passwordInputRef = useRef();
 
-  const switchAuthModeHandler = () => {
-    setIsLogin((prevState) => !prevState);
-  };
+	const authContext = useContext(AuthContext);
 
-  return (
-    <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
-        <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
-        </div>
-        <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
-          <button
-            type='button'
-            className={classes.toggle}
-            onClick={switchAuthModeHandler}
-          >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
-          </button>
-        </div>
-      </form>
-    </section>
-  );
+	const history = useHistory();
+
+	const [isLogin, setIsLogin] = useState(true);
+	const [error, setError] = useState(null);
+	const [isLoading, setIsLoading] = useState(null);
+
+	const switchAuthModeHandler = () => {
+		setIsLogin((prevState) => !prevState);
+	};
+
+	const submitHandler = (event) => {
+		event.preventDefault();
+
+		const enteredEmail = emailInputRef.current.value;
+		const enteredPassword = passwordInputRef.current.value;
+
+		// validation
+
+		setIsLoading(true);
+
+		let url;
+
+		if (isLogin) {
+			url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${appKey}`;
+		} else {
+			url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${appKey}`;
+		}
+
+		fetch(url, {
+			method: 'POST',
+			body: JSON.stringify({
+				password: enteredPassword,
+				email: enteredEmail,
+				returnSecureToken: true
+			}),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+			.then((res) => {
+				setIsLoading(false);
+				if (res.ok) {
+					return res.json();
+				} else {
+					return res.json().then((data) => {
+						let errorMessage = 'Authentication faild!';
+
+						if (data?.error?.message) {
+							errorMessage = data.error.message;
+						}
+
+						throw new Error(errorMessage);
+					});
+				}
+			})
+			.then((data) => {
+				const expirationTime = new Date(
+					new Date().getTime() + +data.expiresIn * 1000
+				);
+
+				authContext.login(data.idToken, expirationTime);
+				history.replace('/');
+			})
+			.catch((error) => {
+				setError(error.message);
+			});
+	};
+
+	const blurHandler = () => {
+		setError(null);
+	};
+
+	return (
+		<section className={classes.auth}>
+			<h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+			<form onSubmit={submitHandler} onBlur={blurHandler}>
+				{error && <div style={{ color: 'red' }}>{error}</div>}
+				<div className={classes.control}>
+					<label htmlFor="email">Your Email</label>
+					<input
+						type="email"
+						id="email"
+						required
+						ref={emailInputRef}
+					/>
+				</div>
+				<div className={classes.control}>
+					<label htmlFor="password">Your Password</label>
+					<input
+						type="password"
+						id="password"
+						required
+						ref={passwordInputRef}
+					/>
+				</div>
+				<div className={classes.actions}>
+					{!isLoading && (
+						<button>{isLogin ? 'Login' : 'Create Account'}</button>
+					)}
+					{isLoading && <p>Sending request...</p>}
+					<button
+						type="button"
+						className={classes.toggle}
+						onClick={switchAuthModeHandler}
+					>
+						{isLogin
+							? 'Create new account'
+							: 'Login with existing account'}
+					</button>
+				</div>
+			</form>
+		</section>
+	);
 };
 
 export default AuthForm;
--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,17 +1,58 @@
+import { useContext, useRef } from 'react';
+import { useHistory } from 'react-router';
+
+import { appKey } from '../../appKey';
+import AuthContext from '../../store/auth-context';
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
-  return (
-    <form className={classes.form}>
-      <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
-      </div>
-      <div className={classes.action}>
-        <button>Change Password</button>
-      </div>
-    </form>
-  );
-}
+	const newPasswordInputRef = useRef();
+
+	const history = useHistory();
+
+	const { token: idToken } = useContext(AuthContext);
+
+	const newPasswordSubmitHandler = (event) => {
+		event.preventDefault();
+
+		const enteredNewPassword = newPasswordInputRef.current.value;
+
+		//validation
+
+		fetch(
+			`https://identitytoolkit.googleapis.com/v1/accounts:update?key=${appKey}`,
+			{
+				method: 'POST',
+				body: JSON.stringify({
+					idToken,
+					password: enteredNewPassword,
+					returnSecureToken: false
+				}),
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			}
+		).then(() => {
+			history.replace('/');
+		});
+	};
+
+	return (
+		<form className={classes.form} onSubmit={newPasswordSubmitHandler}>
+			<div className={classes.control}>
+				<label htmlFor="new-password">New Password</label>
+				<input
+					type="password"
+					id="new-password"
+					minLength="6"
+					ref={newPasswordInputRef}
+				/>
+			</div>
+			<div className={classes.action}>
+				<button>Change Password</button>
+			</div>
+		</form>
+	);
+};
 
 export default ProfileForm;
